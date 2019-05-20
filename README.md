e6ab7a055302a337a18e93b206080b580e155b1c
Andreas Merkle <web@blue-andi.de>
2019-05-20 21:26:45 +0200

    First cli command introduced to get a token and the vehicles.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # niu-cloud-cli
-NIU cloud command line interface for easy scripting.
+Access the NIU cloud via command line interface. It is based on the [niu-cloud-connector](https://github.com/BlueAndi/niu-cloud-connector).
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](http://choosealicense.com/licenses/mit/)
+
+A project originated from an [article in the Elektroroller-Forum](https://www.elektroroller-forum.de/viewtopic.php?f=63&t=6227) [German].
+
+## Installation
+
+```
+$ git clone https://github.com/BlueAndi/niu-cloud-cli.git
+$ cd niu-cloud-cli
+$ npm install
+```
+
+## Show all commands and parameters
+
+```
+node niu-cloud-cli.js --help
+```
+
+## Example
+
+First create a session token with your NIU cloud account. This token is necessary for all further requests.
+
+```
+node niu-cloud-cli.js create-token <account> <password> <country-code>
+```
+
+Get a list of your vehicles. Each vehicle has a serial number, which is necessary to get vehicle specific informations.
+
+```
+node niu-cloud-cli.js get-vehicles --token <token>
+```
+
+## Issues, Ideas and bugs
+
+If you have further ideas or you found some bugs, great! Create a [issue](https://github.com/BlueAndi/niu-cloud-connector/issues) or if
+you are able and willing to fix it by yourself, clone the repository and create a pull request.
+
+## License
+The whole source code is published under the [MIT license](http://choosealicense.com/licenses/mit/).
---
+++ src/getVehicles.js
@@ -0,0 +1,92 @@
+/* MIT License
+ *
+ * Copyright (c) 2019 Andreas Merkle <web@blue-andi.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+const niuCloudConnector = require("../libs/niu-cloud-connector");
+
+exports.command = "get-vehicles";
+
+exports.describe = "Get vehicles.";
+
+exports.builder = {
+    token: {
+        describe: "Token",
+        type: "string",
+        demand: true
+    },
+    json: {
+        describe: "Output results in JSON format.",
+        type: "boolean",
+        default: false
+    }
+};
+
+exports.handler = function(argv) {
+    var client = new niuCloudConnector.Client();
+
+    client.setSessionToken({
+
+        token: argv.token
+
+    }).then(function(result) {
+
+        return client.getVehicles();
+
+    }).then(function(result) {
+
+        var vehicleIndex    = 0;
+        var vehicles        = result.result.data;
+        var vehicle         = null;
+
+        if (true === argv.json) {
+
+            console.log(JSON.stringify(result.result, null, 2));
+
+        } else {
+
+            if (0 === vehicles.length) {
+
+                console.log("No vehicles available.");
+
+            } else {
+
+                for(vehicleIndex = 0; vehicleIndex < vehicles.length; ++vehicleIndex) {
+    
+                    vehicle = vehicles[vehicleIndex];
+        
+                    console.log((vehicleIndex + 1) + ". " + vehicle.type);
+                    console.log("\tName         : " + vehicle.name);
+                    console.log("\tSerial number: " + vehicle.sn);
+                    console.log("\tFrame number : " + vehicle.frameNo);
+                    console.log("\tEngine number: " + vehicle.engineNo);
+                }
+
+            }
+        }
+
+    }).catch(function(err) {
+
+        console.log("Error: ", err.error.message);
+
+    });
+
+    return;
+};
